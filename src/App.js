3a8b75352b1444ca2a52b0fa5f433ab55ea428d8
Apoorv Ahluwalia <apoorv2402@gmail.com>
2021-02-13 15:52:44 +0530

    Getting data from input box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Users from './Components/Users';
 
 function App() {
+  const [text, setText] = useState(null);
+  const [id, setId] = useState(null);
+  const [show, setShow] = useState(false);
+
+ 
+  function updatetext(val) {
+    let date = new Date();
+    setId(date.getTime());
+    setText({name: val.target.value})
+    setShow(false)
+  }
+
+  console.log(text);
+console.log(show)
+console.log(id)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Get Input Box Value !</h1>
+      <input type="text" placeholder="Enter Text" onChange={updatetext} ></input><br /><br />
+      <button onClick={()=>setShow(true)} >Add new User</button>
+              
     </div>
   );
 }
---
+++ src/Components/Users.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Users extends Component {
+    render() {
+        const mystyle = {
+            border: "2px solid black",
+            color: "white",
+            width: "300px",
+            margin :"20px auto",
+            backgroundColor: "Gray",
+            padding: "10px",
+            fontFamily: "Arial"
+          };
+        return (
+            <div style={mystyle}>
+                <h1>New User Added ...!</h1>
+                <h3> Name : {this.props.name}</h3>
+            </div>
+        )
+    }
+}
+
+export default Users
