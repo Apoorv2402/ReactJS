bb3f38a124c91a0885f612505be061231cfa6b79
Apoorv Ahluwalia <apoorv2402@gmail.com>
2021-02-13 16:24:01 +0530

    Users component converted to function

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import Users from './Components/Users';
 
 function App() {
-  const [text, setText] = useState(null);
-  const [id, setId] = useState(null);
+  const [text, setText] = useState();
+  const [id, setId] = useState();
   const [show, setShow] = useState(false);
 
  
   function updatetext(val) {
     let date = new Date();
-    setId(date.getTime());
+    setId(val.target.value + date.getTime());
     setText({name: val.target.value})
     setShow(false)
   }
 
-  console.log(text);
+console.log(text);
 console.log(show)
 console.log(id)
   return (
@@ -23,7 +23,7 @@ console.log(id)
       <h1> Get Input Box Value !</h1>
       <input type="text" placeholder="Enter Text" onChange={updatetext} ></input><br /><br />
       <button onClick={()=>setShow(true)} >Add new User</button>
-              
+        <Users name = {text} userid = {id}/>      
     </div>
   );
 }
--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react'
 
-export class Users extends Component {
-    constructor(){
-     super();
-        
-    }
-    render() {
-        const mystyle = {
-            border: "2px solid black",
-            color: "white",
-            width: "300px",
-            margin :"20px auto",
-            backgroundColor: "Gray",
-            padding: "10px",
-            fontFamily: "Arial"
-          };
-        return (
-            <div style={mystyle}>
-                <h1>New User Added ...!</h1>
-                <h3> Name : {this.props.name}</h3>
-                <h3> User ID: {this.props.userid}</h3>
-            </div>
-        )
-    }
+function Users(props) {
+    const mystyle = {
+        border: "2px solid black",
+        color: "white",
+        width: "300px",
+        margin: "20px auto",
+        backgroundColor: "Gray",
+        padding: "10px",
+        fontFamily: "Arial"
+    };
+    return (
+        <div style={mystyle}>
+            <h1>New User Added ...!</h1>
+            <h3> Name : {props.name}</h3>
+            <h3> User ID: {props.userid}</h3>
+        </div>
+    )
+
 }
 
 export default Users
