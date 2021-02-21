4b11722c776aee1712aaacef44b53bc030f9a3ed
Apoorv Ahluwalia <apoorv2402@gmail.com>
2021-02-21 23:00:39 +0530

    React Routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
+
+import {Button, Card, Accordion} from 'react-bootstrap';
 import Users from './Components/Users';
 
 function App() {
-
-  const [name,setName] = useState('');
-  const [tnc,setTnc] = useState(false);
-  const [plan,setPlan] = useState('');
-  const [newname,setNewname] = useState('');
-  const [newplan,setNewplan] = useState('');
-
-
-  function getFormData(e) {
-    e.preventDefault()
-    if(tnc === true){
-    console.log(name,tnc,plan)
-    setNewname(name)
-    setNewplan(plan)
-    alert('User added succesfully')
-    }else{
-      alert('Agree to T&C`s to continue')
-    }
-  }
-  function deleteUser(e) {
-    setNewname(null)
-    setNewplan(null)
-  }
-;  return (
+  
+  return (
     <div className="App">
-
-  <form onSubmit={getFormData}>
-    <h3>Form Handling in React</h3>
-    <input type="text" name="name" placeholder="Enter Username" onChange={(e)=>setName(e.target.value)}></input><br />
-    <label>Choose Plan:</label>
-    <span><select onChange={(e)=>setPlan(e.target.value)}>
-      <option>Basic</option>
-      <option>Standard</option>
-      <option >Premium</option>
-    </select></span>
-    <br />
-    <input id="checkbox" type="checkbox"onChange={(e)=>setTnc(e.target.checked)}></input><span>Accept all the Terms and conditions</span>
-  <button type="submit" >SignUp</button>
-  </form>
-  <div>
-
-  <Users name={newname} plan={newplan} deleteUser={deleteUser} />
-  </div>
+    
     </div>
-  );
+  )
 }
 
 export default App;
