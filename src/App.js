040000d63d3b12e1964a8a1e2e68c19799e0a101
Apoorv Ahluwalia <apoorv2402@gmail.com>
2021-02-22 11:21:12 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Route,Link } from 'react-router-dom';
-import Users from './Components/Users';
+
 
 function App() {
-  let user = [
-    {id:1, name: 'apoorv', email:'apoorv@gmail.com'},
-    {id:2, name: 'dhimkana', email:'karamana@gmail.com'},
-    {id:3, name: 'karana', email:'karana@gmail.com'},
-    {id:4, name: 'Falana', email:'falana@gmail.com'},
-    {id:6, name: 'kilkana', email:'kilkana@gmail.com'},
-    {id:5, name: 'Chintana', email:'Chinta@gmail.com'},
-]
+  
   return (
-    <div className="App">
-      {user.map((item)=>
-      <div key={item.id}>
-        <Link to={`/user/${item.name}/${item.id}`}>{item.name }</Link>
-      </div>  )}
-      <Route path="/user/:name/:id"><Users /></Route>
-    </div>
+<div className="App">
+  <h2>Post Method with Rest Api</h2>
+</div>
   )
 }
 
